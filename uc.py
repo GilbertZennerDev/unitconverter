998724b4df519cef8110a5201f80fde842a54c30
gzenner <gzenner@student.42luxembourg.lu>
2025-10-24 12:11:38 +0200

    converter

--- uc.py
+++ uc.py
@@ -80,6 +80,9 @@ print("Unit Converter")
 def doround(v):
 	return round(v, 2)
 
+
+#Temperature
+
 def c_f(c):
 	return doround((c * 9/5.0 + 32))
 
@@ -98,9 +101,57 @@ def f_k(f):
 def k_f(k):
 	return doround(c_f(k_c(k)))
 	
+#Time
+
+def d_h(d):
+	return 24 * d
+def h_d(h):
+	return doround(h / 24)
+def h_m(h):
+	return 60 * h
+def m_h(h):
+	return doround(h / 60)
+def m_s(m):
+	return 60 * m
+def s_m(s):
+	return doround(s / 60)
+def d_s(d): # days in seconds = days - hours - minutes - seconds
+	return m_s(h_m(d_h(d)))
+def s_d(s): # seconds in days = seconds - minutes - hours - days
+	return h_d(m_h(s_m(s)))
+	
+#Distance
+'''Length / Distance
+
+1 inch = 2.54 cm
+
+1 foot = 12 inches
+
+1 yard = 3 feet
+
+1 mile = 5280 feet
+
+1 centimeter = 0.01 meter
+
+1 meter = 100 centimeters
+
+1 kilometer = 1000 meters
+
+1 mile ≈ 1.60934 kilometers'''
+
+def inch_cm(i):
+	return i * 2.54
+def cm_inch(c):
+	return doround(c / 2.54)
+
+#Tester
+
 def test(v1, n1, func, n2):
 	print(v1, n1, '=', func(v1), n2)
 	
+
+################################################
+
 test(32, 'C', c_f, 'F')
 test(89.6, 'F', f_c, 'C')
 
@@ -112,3 +163,15 @@ test(305.15, 'K', k_c, 'C')
 
 test(89.6, 'F', f_k, 'K')
 test(305.15, 'K', k_f, 'F')
+
+test(3, 'D', d_h, 'H')
+test(72, 'H', h_d, 'D')
+
+test(5, 'H', h_m, 'M')
+test(300, 'M', m_h, 'H')
+
+test(5, 'm', m_s, 's')
+test(300, 's', s_m, 'm')
+
+test(1, 'd', d_s, 's')
+test(86400, 's', s_d, 'd')
