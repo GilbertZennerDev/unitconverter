60b8b62f56cc8f3f210984f34b5028bcf7fa44ec
gzenner <gzenner@student.42luxembourg.lu>
2025-10-24 13:44:46 +0200

    converter

--- uc.py
+++ uc.py
@@ -80,7 +80,6 @@ print("Unit Converter")
 def doround(v):
 	return round(v, 2)
 
-
 #Temperature
 
 def c_f(c):
@@ -179,13 +178,85 @@ def kg_ton(kg):
 #Tester
 
 def test(v1, n1, func, n2):
-	print(v1, n1, '=', func(v1), n2)
+	print(v1, n1.upper(), '=', func(v1), n2.upper())
 	
+# Parser (The actual Program)
+
+#input: 2 C F -> c_f(2)
+import sys
+
+def chk(u1, v1, u2, v2):
+	return u1 == v1 and u2 == v2
+
+def run():
+	USAGE = "2 C F"
+	if len(sys.argv) != 4:
+		print(USAGE); exit();
+	u1 = sys.argv[2].lower()
+	u2 = sys.argv[3].lower()
+	try:
+		v1 = float(sys.argv[1])
+	except Exception as e:
+		print(e); exit()
+
+	# Conversion mapping logic
+
+	# --- Temperature ---
+	if chk(u1, 'c', u2, 'f'): f = c_f
+	if chk(u1, 'f', u2, 'c'): f = f_c
+	if chk(u1, 'c', u2, 'k'): f = c_k
+	if chk(u1, 'k', u2, 'c'): f = k_c
+	if chk(u1, 'f', u2, 'k'): f = f_k
+	if chk(u1, 'k', u2, 'f'): f = k_f
+
+	# --- Time ---
+	if chk(u1, 'd', u2, 'h'): f = d_h
+	if chk(u1, 'h', u2, 'd'): f = h_d
+	if chk(u1, 'h', u2, 'm'): f = h_m
+	if chk(u1, 'm', u2, 'h'): f = m_h
+	if chk(u1, 'm', u2, 's'): f = m_s
+	if chk(u1, 's', u2, 'm'): f = s_m
+	if chk(u1, 's', u2, 'h'): f = s_h
+	if chk(u1, 'd', u2, 'm'): f = d_m
+	if chk(u1, 'm', u2, 'd'): f = m_d
+	if chk(u1, 'd', u2, 's'): f = d_s
+	if chk(u1, 's', u2, 'd'): f = s_d
+
+	# --- Distance ---
+	if chk(u1, 'inch', u2, 'cm'): f = inch_cm
+	if chk(u1, 'cm', u2, 'inch'): f = cm_inch
+	if chk(u1, 'foot', u2, 'inch'): f = foot_inch
+	if chk(u1, 'inch', u2, 'foot'): f = inch_foot
+	if chk(u1, 'yard', u2, 'feet'): f = yard_feet
+	if chk(u1, 'feet', u2, 'yard'): f = feet_yard
+	if chk(u1, 'mile', u2, 'feet'): f = mile_feet
+	if chk(u1, 'feet', u2, 'mile'): f = feet_mile
+	if chk(u1, 'm', u2, 'cm'): f = m_cm
+	if chk(u1, 'cm', u2, 'm'): f = cm_m
+	if chk(u1, 'km', u2, 'm'): f = km_m
+	if chk(u1, 'm', u2, 'km'): f = m_km
+	if chk(u1, 'mile', u2, 'km'): f = mile_km
+	if chk(u1, 'km', u2, 'mile'): f = km_mile
+
+	# --- Mass / Weight ---
+	if chk(u1, 'ounce', u2, 'gram'): f = ounce_gram
+	if chk(u1, 'gram', u2, 'ounce'): f = gram_ounce
+	if chk(u1, 'pound', u2, 'ounce'): f = pound_ounce
+	if chk(u1, 'ounce', u2, 'pound'): f = ounce_pound
+	if chk(u1, 'kg', u2, 'gram'): f = kg_gram
+	if chk(u1, 'gram', u2, 'kg'): f = gram_kg
+	if chk(u1, 'ton_imp', u2, 'pound'): f = ton_imp_pound
+	if chk(u1, 'pound', u2, 'ton_imp'): f = pound_imp_ton
+	if chk(u1, 'ton', u2, 'kg'): f = ton_kg
+	if chk(u1, 'kg', u2, 'ton'): f = kg_ton
+
+	test(v1, u1, f, u2)
+run()
 
 ################################################
 
-test(1, 'D', d_s, 's')
-test(d_s(1), 's', s_d, 'D')
+#test(1, 'D', d_s, 's')
+#test(d_s(1), 's', s_d, 'D')
 
 #test(1, 'D', d_m, 'min')
 #test(d_m(1), 'min', m_d, 'D')
