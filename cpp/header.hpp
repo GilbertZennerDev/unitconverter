887c2d7c5267572aa53ee2f5615f8444e0b18951
Your Name <you@example.com>
2026-01-18 20:53:56 +0100

    gitignore

--- cpp/header.hpp
+++ cpp/header.hpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 // Helpers
-double doround(const double v);
-void show_units();
-string toLower(const string arg);
+void	show_units();
+double	doround(const double v);
+string	toLower(const string arg);
 
 // Converters
 
@@ -35,30 +35,32 @@ const double s_d(const double s);
 
 // Distance
 
+const double  m_cm(const double m);
+const double  cm_m(const double c);
+const double  m_km(const double m);
+const double  km_m(const double km);
 const double inch_cm(const double i);
 const double cm_inch(const double c);
+const double  km_mile(const double km);
 const double  foot_inch(const double f);
 const double  inch_foot(const double i);
 const double  yard_feet(const double y);
 const double  feet_yard(const double f);
 const double  mile_feet(const double m);
 const double  feet_mile(const double f);
-const double  m_cm(const double m);
-const double  cm_m(const double c);
-const double  km_m(const double km);
-const double  m_km(const double m);
 const double  mile_km(const double mile);
-const double  km_mile(const double km);
 
 // Mass / Weight
 
-const double ounce_gram(const double ounce);
+const double kg_ton(const double kg);
+const double kg_gram(const double kg);
+const double ton_kg(const double ton);
+const double gram_kg(const double gram);
 const double gram_ounce(const double gram);
+const double ounce_gram(const double ounce);
 const double pound_ounce(const double pound);
 const double ounce_pound(const double ounce);
-const double kg_gram(const double kg);
-const double gram_kg(const double gram);
 const double ton_imp_pound(const double ton);
 const double pound_imp_ton(const double pound);
-const double ton_kg(const double ton);
-const double kg_ton(const double kg);
+
+
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,11 +1,11 @@
 #include "header.hpp"
 
-bool chk(const string u1, const string v1, const string u2, const string v2)
+bool chk(const string &u1, const string &v1, const string &u2, const string &v2)
 {
 	return (u1 == v1 and u2 == v2);
 }
 
-void printOutput(const double value)
+void printOutput(const double &value)
 {
 	cout << value << "\n";
 }
@@ -18,7 +18,7 @@ int main(int ac, char **av)//our run() function
 	const string u2 = toLower(av[3]);
 	try
 	{
-		v1 = stof(av[1]);
+		v1 = stod(av[1]);
 	}
 	catch (const exception &e)
 	{
@@ -27,51 +27,52 @@ int main(int ac, char **av)//our run() function
 	cout << "Unit Converter\n";
 	//# --- Temperature ---
 	if (chk(u1, "c", u2, "f")) printOutput(c_f(v1));
-	if (chk(u1, "f", u2, "c")) printOutput(f_c(v1));
-	if (chk(u1, "c", u2, "k")) printOutput(c_k(v1));
-	if (chk(u1, "k", u2, "c")) printOutput(k_c(v1));
-	if (chk(u1, "f", u2, "k")) printOutput(f_k(v1));
-	if (chk(u1, "k", u2, "f")) printOutput(k_f(v1));
+	else if (chk(u1, "f", u2, "c")) printOutput(f_c(v1));
+	else if (chk(u1, "c", u2, "k")) printOutput(c_k(v1));
+	else if (chk(u1, "k", u2, "c")) printOutput(k_c(v1));
+	else if (chk(u1, "f", u2, "k")) printOutput(f_k(v1));
+	else if (chk(u1, "k", u2, "f")) printOutput(k_f(v1));
 
 	//# --- Time ---
-	if (chk(u1, "d", u2, "h")) printOutput(d_h(v1));
-	if (chk(u1, "h", u2, "d")) printOutput(h_d(v1));
-	if (chk(u1, "h", u2, "m")) printOutput(h_m(v1));
-	if (chk(u1, "m", u2, "h")) printOutput(m_h(v1));
-	if (chk(u1, "m", u2, "s")) printOutput(m_s(v1));
-	if (chk(u1, "s", u2, "m")) printOutput(s_m(v1));
-	if (chk(u1, "s", u2, "h")) printOutput(s_h(v1));
-	if (chk(u1, "d", u2, "m")) printOutput(d_m(v1));
-	if (chk(u1, "m", u2, "d")) printOutput(m_d(v1));
-	if (chk(u1, "d", u2, "s")) printOutput(d_s(v1));
-	if (chk(u1, "s", u2, "d")) printOutput(s_d(v1));
+	else if (chk(u1, "d", u2, "h")) printOutput(d_h(v1));
+	else if (chk(u1, "h", u2, "d")) printOutput(h_d(v1));
+	else if (chk(u1, "h", u2, "m")) printOutput(h_m(v1));
+	else if (chk(u1, "m", u2, "h")) printOutput(m_h(v1));
+	else if (chk(u1, "m", u2, "s")) printOutput(m_s(v1));
+	else if (chk(u1, "s", u2, "m")) printOutput(s_m(v1));
+	else if (chk(u1, "s", u2, "h")) printOutput(s_h(v1));
+	else if (chk(u1, "d", u2, "m")) printOutput(d_m(v1));
+	else if (chk(u1, "m", u2, "d")) printOutput(m_d(v1));
+	else if (chk(u1, "d", u2, "s")) printOutput(d_s(v1));
+	else if (chk(u1, "s", u2, "d")) printOutput(s_d(v1));
 
 //	# --- Distance ---
-	if (chk(u1, "inch", u2, "cm")) printOutput(inch_cm(v1));
-	if (chk(u1, "cm", u2, "inch")) printOutput(cm_inch(v1));
-	if (chk(u1, "foot", u2, "inch")) printOutput(foot_inch(v1));
-	if (chk(u1, "inch", u2, "foot")) printOutput(inch_foot(v1));
-	if (chk(u1, "yard", u2, "feet")) printOutput(yard_feet(v1));
-	if (chk(u1, "feet", u2, "yard")) printOutput(feet_yard(v1));
-	if (chk(u1, "mile", u2, "feet")) printOutput(mile_feet(v1));
-	if (chk(u1, "feet", u2, "mile")) printOutput(feet_mile(v1));
-	if (chk(u1, "m", u2, "cm")) printOutput(m_cm(v1));
-	if (chk(u1, "cm", u2, "m")) printOutput(cm_m(v1));
-	if (chk(u1, "km", u2, "m")) printOutput(km_m(v1));
-	if (chk(u1, "m", u2, "km")) printOutput(m_km(v1));
-	if (chk(u1, "mile", u2, "km")) printOutput(mile_km(v1));
-	if (chk(u1, "km", u2, "mile")) printOutput(km_mile(v1));
+	else if (chk(u1, "inch", u2, "cm")) printOutput(inch_cm(v1));
+	else if (chk(u1, "cm", u2, "inch")) printOutput(cm_inch(v1));
+	else if (chk(u1, "foot", u2, "inch")) printOutput(foot_inch(v1));
+	else if (chk(u1, "inch", u2, "foot")) printOutput(inch_foot(v1));
+	else if (chk(u1, "yard", u2, "feet")) printOutput(yard_feet(v1));
+	else if (chk(u1, "feet", u2, "yard")) printOutput(feet_yard(v1));
+	else if (chk(u1, "mile", u2, "feet")) printOutput(mile_feet(v1));
+	else if (chk(u1, "feet", u2, "mile")) printOutput(feet_mile(v1));
+	else if (chk(u1, "m", u2, "cm")) printOutput(m_cm(v1));
+	else if (chk(u1, "cm", u2, "m")) printOutput(cm_m(v1));
+	else if (chk(u1, "km", u2, "m")) printOutput(km_m(v1));
+	else if (chk(u1, "m", u2, "km")) printOutput(m_km(v1));
+	else if (chk(u1, "mile", u2, "km")) printOutput(mile_km(v1));
+	else if (chk(u1, "km", u2, "mile")) printOutput(km_mile(v1));
 
 //	# --- Mass / Weight ---
-	if (chk(u1, "ounce", u2, "gram")) printOutput(ounce_gram(v1));
-	if (chk(u1, "gram", u2, "ounce")) printOutput(gram_ounce(v1));
-	if (chk(u1, "pound", u2, "ounce")) printOutput(pound_ounce(v1));
-	if (chk(u1, "ounce", u2, "pound")) printOutput(ounce_pound(v1));
-	if (chk(u1, "kg", u2, "gram")) printOutput(kg_gram(v1));
-	if (chk(u1, "gram", u2, "kg")) printOutput(gram_kg(v1));
-	if (chk(u1, "ton_imp", u2, "pound")) printOutput(ton_imp_pound(v1));
-	if (chk(u1, "pound", u2, "ton_imp")) printOutput(pound_imp_ton(v1));
-	if (chk(u1, "ton", u2, "kg")) printOutput(ton_kg(v1));
-	if (chk(u1, "kg", u2, "ton")) printOutput(kg_ton(v1));
+	else if (chk(u1, "ounce", u2, "gram")) printOutput(ounce_gram(v1));
+	else if (chk(u1, "gram", u2, "ounce")) printOutput(gram_ounce(v1));
+	else if (chk(u1, "pound", u2, "ounce")) printOutput(pound_ounce(v1));
+	else if (chk(u1, "ounce", u2, "pound")) printOutput(ounce_pound(v1));
+	else if (chk(u1, "kg", u2, "gram")) printOutput(kg_gram(v1));
+	else if (chk(u1, "gram", u2, "kg")) printOutput(gram_kg(v1));
+	else if (chk(u1, "ton_imp", u2, "pound")) printOutput(ton_imp_pound(v1));
+	else if (chk(u1, "pound", u2, "ton_imp")) printOutput(pound_imp_ton(v1));
+	else if (chk(u1, "ton", u2, "kg")) printOutput(ton_kg(v1));
+	else if (chk(u1, "kg", u2, "ton")) printOutput(kg_ton(v1));
+	else cout << "Invalid Input.\n";
 	return (0);
 }
